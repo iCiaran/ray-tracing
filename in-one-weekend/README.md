3f11acc66e01ff90ff838bcf7858f8330a8da9e9
iCiaran <iCiaran@users.noreply.github.com>
2020-05-20 00:42:11 +0100

    Metal fuzzy reflection
    9.6

--- in-one-weekend/README.md
+++ in-one-weekend/README.md
@@ -35,13 +35,13 @@
   - [x] 8.4  Fixing Shadow Acne
   - [x] 8.5  True Lambertian Reflection
   - [x] 8.6  An Alternative Diffuse Formulation
-- [ ] 9  Metal
+- [x] 9  Metal
   - [x] 9.1  An Abstract Class for Materials
   - [x] 9.2  A Data Structure to Describe Ray-Object Intersections
   - [x] 9.3  Modeling Light Scatter and Reflectance
   - [x] 9.4  Mirrored Light Reflection
   - [x] 9.5  A Scene with Metal Spheres
-  - [ ] 9.6  Fuzzy Reflection
+  - [x] 9.6  Fuzzy Reflection
 - [ ] 10  Dielectrics
   - [ ] 10.1  Refraction
   - [ ] 10.2  Snell's Law
--- maths/metal.go
+++ maths/metal.go
@@ -2,15 +2,16 @@ package maths
 
 type Metal struct {
 	Albedo *Colour
+	fuzz   float64
 }
 
-func NewMetal(albedo *Colour) *Metal {
-	return &Metal{albedo}
+func NewMetal(albedo *Colour, fuzz float64) *Metal {
+	return &Metal{albedo, Clamp(fuzz, 0.0, 1.0)}
 }
 
 func (m *Metal) Scatter(rIn *Ray, rec *HitRecord, attenuation *Colour, scattered *Ray) bool {
 	reflected := Reflect(Normalise(rIn.Direction()), rec.Normal)
-	*scattered = *NewRay(rec.P, reflected)
+	*scattered = *NewRay(rec.P, reflected.Add(Mul(RandomInUnitSphere(), m.fuzz)))
 	*attenuation = *m.Albedo
 
 	return Dot(scattered.Direction(), rec.Normal) > 0
