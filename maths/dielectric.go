4596b0fb90550bf14ed40a62b149c84666b7a224
iCiaran <iCiaran@users.noreply.github.com>
2020-05-20 23:46:25 +0100

    Total internal reflection
    10.3

--- maths/dielectric.go
+++ maths/dielectric.go
@@ -1,5 +1,7 @@
 package maths
 
+import "math"
+
 type Dielectric struct {
 	refIdx float64
 }
@@ -17,6 +19,14 @@ func (d *Dielectric) Scatter(rIn *Ray, rec *HitRecord, attenuation *Colour, scat
 	}
 
 	unitDirection := Normalise(rIn.Direction())
+	cosTheta := math.Min(Dot(Neg(unitDirection), rec.Normal), 1.0)
+	sinTheta := math.Sqrt(1.0 - cosTheta*cosTheta)
+	if etaiOverEtat*sinTheta > 1.0 {
+		reflected := Reflect(unitDirection, rec.Normal)
+		*scattered = *NewRay(rec.P, reflected)
+		return true
+	}
+
 	refracted := Refract(unitDirection, rec.Normal, etaiOverEtat)
 	*scattered = *NewRay(rec.P, refracted)
 	return true
