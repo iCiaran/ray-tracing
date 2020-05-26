b29e7bd475b440f5626d7b57a2ce89a089864628
iCiaran <iCiaran@users.noreply.github.com>
2020-05-27 00:12:39 +0100

    Faster hit

--- maths/aabb.go
+++ maths/aabb.go
@@ -1,7 +1,5 @@
 package maths
 
-import "math"
-
 type AABB struct {
 	min *Point3
 	max *Point3
@@ -13,12 +11,22 @@ func NewAABB(a, b *Point3) *AABB {
 
 func (a *AABB) Hit(r *Ray, tMin, tMax float64) bool {
 	for i := 0; i < 3; i++ {
-		t0 := math.Min(a.min[i]-r.Origin()[i]/r.Direction()[i],
-			a.max[i]-r.Origin()[i]/r.Direction()[i])
-		t1 := math.Max(a.min[i]-r.Origin()[i]/r.Direction()[i],
-			a.max[i]-r.Origin()[i]/r.Direction()[i])
-		tMin = math.Max(t0, tMin)
-		tMax = math.Min(t1, tMax)
+		invD := 1.0 / r.Direction()[i]
+		t0 := invD * (a.min[i] - r.Origin()[i])
+		t1 := invD * (a.max[i] - r.Origin()[i])
+
+		if invD < 0.0 {
+			t0, t1 = t1, t0
+		}
+
+		if t0 > tMin {
+			tMin = t0
+		}
+
+		if t1 < tMax {
+			tMax = t1
+		}
+
 		if tMax <= tMin {
 			return false
 		}
